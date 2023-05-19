5cd63a3d37e8f2818dad36fc3c2082a58b6b75a1
Trần Thanh Tùng <tthanhtung92@gmail.com>
2023-05-19 20:26:29 +0700

    done5

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Link from "next/link";
+import Button from "../components/Button";
 import { useRef } from "react";
 import { stagger } from "../animations";
 import Footer from "../components/Footer";
